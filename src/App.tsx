dcbca2ed498a9edd8c1a26eb4efc8a873a47cb9d
jonasmoeneclaey <jonas.moeneclaey@student.tsaam.be>
2026-03-31 11:45:47 +0200

    Hierbij dien ik in.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import RegistrationForm from "./components/RegistrationForm";
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import UserList from "./components/UserList/UserList";
 
 export default function App() {
@@ -7,14 +7,14 @@ export default function App() {
     "Karel",
     "Els",
     "Piet",
-  ]);
+  ]); // ik maak een useState van mijn array zodat ik hem kan aanpassen.
 
   return (
     <div>
       <RegistrationForm
-        onRegister={(naam: string) => {
-          setGeregistreerdePersonen([...GeregistreerdePersonen, naam]);
-          console.log(GeregistreerdePersonen);
+        onRegister={(naam: string) => { // hier maak ik gebruik van de vooraf gemaakte prop functie die ervoor zorgt dat ik het gegeven van het invulveld terug krijg
+          setGeregistreerdePersonen([...GeregistreerdePersonen, naam]); // met deze zin code voeg ik de ingevulde naam toe aan de array die gegeven werd.
+          console.log(GeregistreerdePersonen); // hier wil ik een log van de array waarbij er iets aan werd toegevoegd.
         }}
       />
       <hr />
